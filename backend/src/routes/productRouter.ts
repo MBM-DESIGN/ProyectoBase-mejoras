1a763e6f065cb9d82913eb9ebff40eab58b8632a
MBM-DESIGN <mariabelenmarchi@gmail.com>
2025-07-19 14:57:12 -0300

    5°commit: actualización en las rutas de los productos

--- backend/src/routes/productRouter.ts
+++ backend/src/routes/productRouter.ts
@@ -1,12 +1,15 @@
 import { Router } from "express"
-import { addNewProduct, getAllProducts, deleteProduct, updateProduct } from "../controllers/productController"
+import { addNewProduct, getAllProducts, deleteProduct, updateProduct,searchProducts } from "../controllers/productController" //importamos la nueva función
+import { protect } from "../middleware/auth";
 
-const productRouter = Router()
+//const productRouter = Router()
+export const productRouter = Router();
 
 // manejar las peticiones para los productos
-productRouter.get("/", getAllProducts)
-productRouter.post("/", addNewProduct)
-productRouter.patch("/:id", updateProduct)
-productRouter.delete("/:id", deleteProduct)
+productRouter.get("/", protect, getAllProducts)
+productRouter.post("/", protect, addNewProduct)
+productRouter.get("/search/:query", protect, searchProducts); //lo colocamos antes de la ruta /:id para evitar conflictos de especificidad
+productRouter.patch("/:id", protect, updateProduct)
+productRouter.delete("/:id", protect, deleteProduct)
 
-export { productRouter }
+//export { productRouter }
